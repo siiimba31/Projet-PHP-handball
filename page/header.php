4f77b7cedb03d0a9dd1cdb1bc4e67febca88cf1a
siiimba31 <elena.chelle31@gmail.com>
2023-01-10 11:33:17 +0100

    ajout de page + tableau joueur
    première ébauche d'un tableau

--- page/header.php
+++ page/header.php
@@ -18,9 +18,9 @@
         <nav id="navmenu">
             <ul id="ulmenu">
                 <li class="limenu"><a href=accueil.php>ACCUEIL</a></li>
-                <li class="limenu"><a href=presentation_BUT.html>JOUEURS</a></li>
-                <li class="limenu"><a href=Competence.html>MATCH</a></li>
-                <li class="limenu"><a href=contact.html>STATISTIQUE</a></li>
+                <li class="limenu"><a href=joueur.php>JOUEURS</a></li>
+                <li class="limenu"><a href=match.php>MATCH</a></li>
+                <li class="limenu"><a href=statistique.php>STATISTIQUE</a></li>
                 <li class="limenu"><a href=deconnexion.php>DECONNEXION</a></li>
             </ul>
             <img id="logo" src="../source/img/logo.png" alt="logo équipe">
--- page/joueur.php
+++ page/joueur.php
@@ -5,7 +5,41 @@ utilisateur_connecte();
 
 // on appelle la page header qui vas nous afficher l'entete 
 //et le le nom de notre page avec la variable $title
-$title='Accueil';
+$title='Joueur';
 require 'header.php';
 ?>
 
+
+<?php
+require './../source/fonctionPHP/connexionbd.php';
+$requete = $linkpdo->query("SELECT `num_licence`, `nom`, `prénom`, `statut` FROM `joueur`, `Statuts` where Statuts.ID_statut=joueur.id_statut ");
+
+?>
+
+<div class="contenaire-tableau">
+<h1> Joueur du club </h1>
+
+<table>
+        <tr>
+            <th> Licence </th>
+            <th> nom </th>
+            <th> prenom </th>
+            <th> statut </th>
+        </tr>
+<?php
+while($resultat = $requete->fetch()):
+    ?>
+    <tr>
+          <td> <?php echo $resultat['num_licence'] ?> </td>
+          <td> <?php echo $resultat['nom'] ?> </td>
+          <td> <?php echo $resultat['prénom'] ?> </td>
+          <td> <?php echo $resultat['statut'] ?> </td>
+    </tr>
+    <?php
+endwhile;
+?>
+
+
+</table>
+        
+</div>
